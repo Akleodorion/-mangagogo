90668d0c6425d9138176d26ee2a94266cb542945
philippe goulliart <philippe.goulliart62@gmail.com>
2023-02-28 12:32:28 +0100

    seeding implemented and tested

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+require "json"
+require "open-uri"
+
+API_URL = 'https://api.jikan.moe/v4/manga/'
+
+20.times do
+  random_id = rand(1..250)
+  url = "https://api.jikan.moe/v4/manga/#{random_id}"
+  json_data = URI.open(url)
+  data = JSON.parse(json_data)
+
+  # manga require saga, volume, description, and number
+  saga = data["data"]["titles"][0]["title"]
+  picture = data["data"]["images"]["jpg"]["small_image_url"]
+  volume = rand(1..data["data"]["volumes"].to_i)
+  description = data["data"]["synopsis"]
+
+  new_manga = Manga.new(saga: saga, picture: picture, volume: volume, description: description)
+  new_manga.save!
+end
