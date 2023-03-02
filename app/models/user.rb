b335a1b645f07a72b0c5ecb7ed9129b55ba0b2aa
philippe goulliart <philippe.goulliart62@gmail.com>
2023-03-02 16:36:23 +0100

    pour christian

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_many :mangas, dependent: :destroy
   has_many :bookings
-  has_many :bookings_as_owner, trough: :mangas, source: :bookings
+  has_many :bookings_as_owner, through: :mangas, source: :bookings
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
