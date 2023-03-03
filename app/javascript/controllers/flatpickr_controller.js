56a0ec5a3c8a959528b5e72b1480402c51395878
jeremy twardowski <kherset@protonmail.com>
2023-03-03 11:38:19 +0100

    Calendar ok date anterieure bloquee

--- app/javascript/controllers/flatpickr_controller.js
+++ app/javascript/controllers/flatpickr_controller.js
@@ -7,7 +7,7 @@ export default class extends Controller {
   static targets = [ "startDate", "endDate" ]
   connect() {
     flatpickr(this.startDateTarget, {
-      enableTime: true,
+      enableTime: true, minDate: "today",
       // Provide an id for the plugin to work
       plugins: [new rangePlugin({ input: "#end_time"})]})
 flatpickr(this.endDateTarget, {})
